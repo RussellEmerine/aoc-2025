133294ecad17c13c91fb1b5c5cd6fa20d2414dea
Russell Emerine <rre0520@gmail.com>
2026-01-24 07:22:22 -0800

    Added proofs for Day 1

--- Aoc2025/Day01.lean
+++ Aoc2025/Day01.lean
@@ -1,4 +1,7 @@
-import Std.Internal.Parsec.String 
+import Std.Internal.Parsec.String
+import Mathlib.Logic.Function.Iterate
+import Mathlib.Data.Nat.ModEq
+import Mathlib.Data.ZMod.Defs -- NOTE: defines ring on Fin
 
 open Std.Internal.Parsec.String
 
@@ -51,10 +54,88 @@ def step (dial : Dial) (dir : Dir) : Dial :=
 
 def step_many (dial : Dial) (rot : Rot) : Dial :=
   let cycles := rot.dist / 100
-  (rot.dist % 100).repeat (step · rot.dir) { pos := dial.pos, count := dial.count + cycles }
+  (step · rot.dir)^[rot.dist % 100] { pos := dial.pos, count := dial.count + cycles }
 
--- TODO: 100.repeat (step · rot.dir) dial = { pos := dial.pos, count := dial.count + 1 }
--- TODO: step_many dial rot = rot.dist.repeat (step · rot.dir) dial 
+lemma range_countP_eq_one
+  {n : ℕ} [NeZero n]
+  (x : Fin n)
+: (List.range n).countP (Fin.ofNat n · = x) = 1 := by
+  have h : ∀ y ∈ List.range n, decide (Fin.ofNat n y = x) ↔ decide (y = x) = true := by
+    intro y hy
+    rw [Bool.decide_iff, Bool.decide_iff]
+    rw [List.mem_range] at hy
+    refine ⟨?_, ?_⟩
+    · intro rfl
+      rw [Fin.val_ofNat, Nat.mod_eq_of_lt hy]
+    · intro rfl
+      rw [Fin.ofNat_val_eq_self]
+  rw [List.countP_congr h]
+  conv =>
+    lhs
+    congr
+    ext
+    rw [← beq_eq_decide]
+  rw [← List.count_eq_countP, List.count_range]
+  simp
+
+lemma iterate_step
+: (step · dir)^[n] dial = {
+    pos := dial.pos + Fin.ofNat _ n * dir.delta,
+    count := dial.count + (List.range n).countP (dial.pos + Fin.ofNat _ · * dir.delta + dir.delta = 0)
+  }
+:= by
+  induction n
+  case zero =>
+    simp [-Fin.ofNat_eq_cast]
+  case succ n hn =>
+    rw [add_comm, Function.iterate_add_apply, Function.iterate_one, hn, step]
+    congr 1
+    · rw [add_comm 1 n]
+      simp [-Fin.ofNat_eq_cast, Fin.ofNat_mul, Fin.ofNat_add, Fin.add_ofNat, add_mul, add_assoc]
+    · simp [Nat.one_add, List.range_succ, List.countP_append, List.countP_singleton, add_assoc]
+
+lemma iterate_step_100_eq_add_one
+: (step · dir)^[100] dial = { pos := dial.pos, count := dial.count + 1 } := by
+  rw [iterate_step]
+  simp [-Fin.ofNat_eq_cast]
+  cases dir
+  case L =>
+    simp [-Fin.ofNat_eq_cast, Dir.delta]
+    conv =>
+      lhs
+      congr
+      ext
+      congr
+      rw [add_eq_zero_iff_eq_neg, ← eq_sub_iff_add_eq', neg_eq_iff_eq_neg, neg_sub, neg_neg]
+    rw [range_countP_eq_one]
+  case R =>
+    simp [-Fin.ofNat_eq_cast, Dir.delta]
+    conv =>
+      lhs
+      congr
+      ext
+      congr
+      rw [add_eq_zero_iff_eq_neg, ← eq_neg_add_iff_add_eq]
+    rw [range_countP_eq_one]
+
+lemma add_cycles_eq_iterate_step_mul_100
+: { pos := dial.pos, count := dial.count + cycles } = (step · dir)^[cycles * 100] dial := by
+  induction cycles
+  case zero =>
+    simp
+  case succ n hn =>
+    rw [add_mul, one_mul, add_comm _ 100, Function.iterate_add_apply, ← hn]
+    rw [iterate_step_100_eq_add_one]
+    simp [add_assoc]
+
+theorem step_many_eq_iterate_step : step_many dial rot = (step · rot.dir)^[rot.dist] dial := by
+  rcases rot with ⟨dir, dist⟩
+  dsimp [step_many]
+  conv =>
+    rhs
+    arg 2
+    rw [← dist.mod_add_div 100, mul_comm]
+  rw [Function.iterate_add_apply, add_cycles_eq_iterate_step_mul_100]
 
 end Dial
 
